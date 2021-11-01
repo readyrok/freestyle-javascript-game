06eeb860ddc2b66772240d18eda11ea2ea076fb7
Stefanescu Radu <radu98fm@gmail.com>
2021-11-01 15:00:43 +0200

    started

--- game.js
+++ game.js
@@ -5,3 +5,16 @@ function initGame() {
     // Your game can start here, but define separate functions, don't write everything in here :)
 
 }
+
+const play = document.querySelector('button')
+const test = play.getBoundingClientRect()
+console.log(test)
+// function addBackground(){
+//     document.body.style.backgroundImage = "url('background.jpg')"
+// }
+
+// play.addEventListener('click', () => addBackground())
+play.addEventListener('keyup', (event) => {
+    console.log(event.clientX)
+   
+  })
