883c956cb52d760cf62f16fb6c8a0993a353d071
readyrok <sebastian.techera015@gmail.com>
2021-11-02 10:43:12 +0200

    Started working on menu

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 initGame();
 
 function initGame() {
-
+    // playGame();
     // Your game can start here, but define separate functions, don't write everything in here :)
 
 }
@@ -18,3 +18,17 @@ play.addEventListener('keyup', (event) => {
     console.log(event.clientX)
    
   })
+
+
+function playGame(){
+    let playerName = document.querySelector('#playerName').value;
+    
+    console.log(button)
+    document.body.style.backgroundImage = '';
+
+}
+
+let button = document.querySelector("#submit");
+button.addEventListener('submit', (event) => {
+    console.log(event)
+})
